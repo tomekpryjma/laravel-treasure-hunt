9628ca158e3e380042a7a01e0c15a3b4ef7c15e6
tomekpryjma <tomek.pryjma@gmail.com>
2023-05-13 17:19:55 +0100

    Refactor game session authentication

--- app/Http/Controllers/GameSessionController.php
+++ app/Http/Controllers/GameSessionController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\GameSession;
+use App\Services\GameSessionAuthService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class GameSessionController extends Controller
 {
+    private $gameSessionAuthService;
+
+    public function __construct(GameSessionAuthService $gameSessionAuthService)
+    {
+        $this->gameSessionAuthService = $gameSessionAuthService;
+    }
     public function lobby(Request $request, $sessionCode)
     {
         $gameSession = GameSession::byCode($sessionCode)->first();
@@ -21,22 +28,39 @@ class GameSessionController extends Controller
         ]);
     }
 
-    public function show(Request $request, $sessionCode)
+    public function attemptAccess(Request $request, $sessionCode)
     {
         $validator = Validator::make($request->all(), [
-            'access_code' => ['required', 'numeric'],
+            'access_code' => ['required', 'numeric', 'max_digits:20'],
         ]);
 
         if ($validator->fails()) {
+            $this->gameSessionAuthService->removeAuth();
             return back()->withErrors($validator->errors());
         }
 
         $gameSession = GameSession::byCode($sessionCode)
             ->where('access_code', $request->input('access_code'))
-            ->with('game')
             ->first();
 
         if (!$gameSession) {
+            abort(404);
+        }
+
+        $this->gameSessionAuthService->storeSessionAccessCode($gameSession->access_code);
+
+        return redirect()->route('game-session.show', [
+            'sessionCode' => $sessionCode,
+        ]);
+    }
+
+    public function show(Request $request, $sessionCode)
+    {
+        $gameSession = GameSession::byCode($sessionCode)
+            ->with('game')
+            ->first();
+
+        if (!$this->gameSessionAuthService->verifySessionAccessCode($gameSession)) {
             abort(404);
         }
 
